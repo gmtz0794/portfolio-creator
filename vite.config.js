4cd49887f29bb1672bc8bdd10c66303da2fdfd51
Gustavo Martinez <gmtz.0794@gmail.com>
2024-12-08 22:53:31 -0800

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3004,
+//     open: true
+//   }
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3004,
-    open: true
-  }
-})
+  build: {
+    outDir: 'dist', // Default output folder
+  },
+  base: '/', // Use '/' for a root domain or adjust if deploying to a subdirectory
+});
